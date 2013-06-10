2a341b733fa8ee5c97d338e5ee44dfa6ac1db1dc
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-06-10 12:35:13 -0400

    Added code to move today's file to main dir, also fixed indices to create a file for today, instead of starting with tomorrow

--- python/one_a_day.py
+++ python/one_a_day.py
@@ -10,26 +10,49 @@ import commands
 import shutil
 import os
 import datetime
+import random
 
 os.chdir(os.path.realpath(__file__).replace("one_a_day.py", ""))
 target_dir = 'papers/'
 
 #Execute
 
-i = 1
+filename_format = "%Y-%b-%d-%a"
+i = 0
 
+#Process all pdf files in current dir, give them a future date
 ls = os.listdir(os.curdir)
 for item in ls:
 	if item[-4:] == ".pdf" or item[-4:] == ".PDF":
-		filedone = 0
-		while filedone == 0:
-			toname = datetime.date.today() + datetime.timedelta(days=i)
-			toname = toname.strftime("%Y-%b-%d-%a") + ".pdf"
+		if item != "TODAY.pdf":
+			filedone = 0
+			while filedone == 0:
+				toname = datetime.date.today() + datetime.timedelta(days=i)
+				toname = toname.strftime(filename_format) + ".pdf"
 
-			if os.path.isfile(target_dir + toname):
-				i = i + 1
-			else:
-				shutil.move(item, target_dir + toname)
-				filedone = 1
+				if os.path.isfile(target_dir + toname):
+					i = i + 1
+				else:
+					shutil.move(item, target_dir + toname)
+					filedone = 1
+
+
+#Check if there is one for today already, if not get the next one
+todayfile = "TODAY.pdf"
+if os.path.isfile(todayfile):
+	print "File ready"
+else:
+	i=0
+	filedone = 0
+	while filedone == 0:
+		toname = datetime.date.today() + datetime.timedelta(days=i)
+		toname = toname.strftime(filename_format) + ".pdf"
+
+		if os.path.isfile(target_dir + toname):
+			print target_dir + toname + "\n"
+			shutil.move(target_dir + toname, todayfile)
+			filedone = 1
+		else:
+			i = i + 1
 
 
