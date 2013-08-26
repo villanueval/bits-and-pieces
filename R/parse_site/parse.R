5da9d7f4499af5652178389f153a57756fdb15b7
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-08-26 19:02:15 -0400

    Results now are saved to a single file.

--- R/parse_site/parse.R
+++ R/parse_site/parse.R
@@ -2,24 +2,27 @@ library(XML)
 library(plyr)
 library(RCurl)
 
+#Read list of states
 states <- read.csv("states.csv")
 
+#where to save the data
+writefile <- "results.csv"
+
 for(i in 1:dim(states)[1]){
-#for(i in 1:1){
+#for(i in 1:2){
   #Get states
   thisstate <- as.character(states[i,2])
   
   #Get the list of available HUCs for this state
   thisindex = htmlParse(getURL(paste("http://nas.er.usgs.gov/queries/huc8.aspx?state=", thisstate, sep="")))
 
-  dir.create(path=thisstate)
+  #dir.create(path=thisstate)
   
   #Parse the area tags that contain the HUC code and names
   theseareas <- xpathApply(thisindex, "//area[@shape = 'polygon']")
-  
   HUC <- data.frame(matrix(NA, nrow = 1, ncol = 2))
-  
   for(j in 1:length(theseareas)){
+    #Get the "title" attribute that contains the HUC and the name
     HUC <- rbind(HUC, strsplit(xmlGetAttr(theseareas[[j]], name="title"), ", ")[[1]])
   }
   
@@ -32,12 +35,29 @@ for(i in 1:dim(states)[1]){
     
     thistable <- readHTMLTable(thispage)
     
-    writefile <- paste(thisstate, "/", thisstate, "_", realHUC[h,1], "_", realHUC[h,2], ".csv", sep="")
+    #If the HUC has data, then write to file, otherwise don't do anything
+    if(!is.null(thistable$ctl00_ContentPlaceHolder1_myGridView)){
+      #Add the HUC and name to the data.frame
+      tabletowrite <- cbind(thistable$ctl00_ContentPlaceHolder1_myGridView, thisstate, realHUC[h,1], realHUC[h,2])
       
-    write.table(thistable$ctl00_ContentPlaceHolder1_myGridView, file=writefile, sep=",", row.names=FALSE, col.names=TRUE)
+      names(tabletowrite) <- c(names(tabletowrite)[1:8], "State", "HUC", "Name")
       
+      #Only use these columns
+      keeps <- c("Group", "Family", "Scientific Name", "Common Name", "Native Habitat", "Exotic / Native Transplant", "State", "HUC", "Name")
+      tabletowrite <- tabletowrite[keeps]
+      
+      #writefile <- paste(thisstate, "/", thisstate, "_", realHUC[h,1], "_", realHUC[h,2], ".csv", sep="")
+      
+      #If result file doesn't exist, create and add column names on first row
+      if (!file.exists(writefile)){
+        write.table(tabletowrite, file=writefile, sep=",", row.names=FALSE, col.names=TRUE, append=TRUE)
+      }else{
+        #Just append data, don't add column names
+        write.table(tabletowrite, file=writefile, sep=",", row.names=FALSE, col.names=FALSE, append=TRUE)
+        }
+      }
   }
   #convert to windows-type text file
-  system(paste("todos ", thisstate, "\\/*", sep=""))
+  system(paste("todos ", writefile, sep=""))
 }
 
