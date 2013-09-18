0fd634933b3629d45ee30ba5ac126c52272fd930
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-09-18 14:15:15 -0400

    Adding script to export from pumilio git to several sites

---
+++ bash/git_to_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to export the pumilio git archive to several installations in the server and update it using wget
+
+mkdir 1
+cp -r pumilio/www/* 1/
+
+rm -r 1/install
+rm -r 1/tmp
+rm -r 1/sounds
+
+for site in site1 site2 site3
+do
+  cp -rf 1/* /var/www/$site/
+  wget -O /dev/null http://server/$site/upgrade
+done
+
+rm -r 1
