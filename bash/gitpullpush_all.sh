246f59daca10bc8b15bed9785aa2d89cc76e0ee4
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-09-19 14:38:28 -0400

    Adding script to do add, commit, push, and pull in all git folders

---
+++ bash/gitpullpush_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#Bash script that goes to every directory in the current path and does a git commit, push, and then pull.
+# Useful when your git clones are all in the same directory and work from different workstations.
+#
+for gitarchive in *; do
+   if [ -d $gitarchive ]; then
+      cd $gitarchive
+      git add -A
+      git commit -a
+      git push origin master
+      git pull
+      cd ..
+   fi
+done
