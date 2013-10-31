1c4b5a8c209289c655b9f98737e56f2deb1e1217
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-10-31 11:17:49 -0400

    Adding small pause to see if it reduces timeouts

--- python/pumilio_to_WinHPC/wrapper.py
+++ python/pumilio_to_WinHPC/wrapper.py
@@ -3,6 +3,7 @@
 # Import modules
 from subprocess import call
 import sys
+import time
 
 #Call the script giving one arguments
 steps=int(sys.argv[1])
@@ -21,3 +22,4 @@ while (start < end):
 		print "WinHPC_XMLCreator_Minutes_Local.py " + str(start) + " " + str(start + steps)
 		call(["./WinHPC_XMLCreator_Minutes_Local.py", str(start), str(start + steps)])
 		start = start + steps
+		time.sleep(4)
