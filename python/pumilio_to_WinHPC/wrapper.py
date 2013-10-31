0ec187031e1e596ba692f0ce62bb937cb430a52d
Luis J. Villanueva <ljvillanueva@coquipr.com>
2013-10-31 11:12:34 -0400

    Adding a wrapper for the script to generate XML files for the WinHPC. In steps to avoid job timeouts.

---
+++ python/pumilio_to_WinHPC/wrapper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+# Import modules
+from subprocess import call
+import sys
+
+#Call the script giving one arguments
+steps=int(sys.argv[1])
+end=int(sys.argv[2])
+
+
+
+##########################################################
+# RUN
+##########################################################
+
+start = 1
+
+while (start < end):
+	for i in range(start, start + steps):
+		print "WinHPC_XMLCreator_Minutes_Local.py " + str(start) + " " + str(start + steps)
+		call(["./WinHPC_XMLCreator_Minutes_Local.py", str(start), str(start + steps)])
+		start = start + steps
